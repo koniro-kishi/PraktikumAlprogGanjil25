cb310ddf3ede709b1a8e99cf1fa608d5bea74039
walidan-nadilaw <sirsnakeson@gmail.com>
2025-09-22 22:21:35 +0700

    feat : merge jawaban syafiq & walid

--- Pertemuan5/clean/051_merge2.c
+++ Pertemuan5/clean/051_merge2.c
@@ -6,14 +6,14 @@ int main(){
     int counter1 = 0;
     int counter2 = 0;
 
-    for(int i=0; i<=2000; i++){
+    for(int i=0; i<2001; i++){
         int inputArr1; scanf("%d", &inputArr1);
         if(inputArr1 == -9) break;
         arr1[counter1] = inputArr1;
         counter1++;
     }
 
-    for(int i=0; i<=2000; i++){
+    for(int i=0; i<2001; i++){
         int inputArr2; scanf("%d", &inputArr2);
         if(inputArr2 == -9) break;
         arr2[counter2] = inputArr2;
@@ -38,6 +38,7 @@ int main(){
             printf("%d ", arr1[i1]);
             i1++;
         }
+        
         else if(i2 < counter2){
             printf("%d ", arr2[i2]);
             i2++;
