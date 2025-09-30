d551fb03085c7d8579aa50e4d59001615be908f6
walidan-nadilaw <sirsnakeson@gmail.com>
2025-09-30 16:25:52 +0700

    feat( soal 061) add jawaban soal 61

---
+++ Pertemuan6/clean/061_AngkaKeramat47.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n; scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int counter = 0;
+
+    for (int i = 0; i < n; i++){
+        int sum = 0;
+
+        for (int j = i; j < n; j++){
+            sum += arr[j];
+            if (sum > 47) break;
+            if (sum == 47){
+                counter++;
+            }
+        }
+    }
+
+    printf("%d\n", counter);
+}
